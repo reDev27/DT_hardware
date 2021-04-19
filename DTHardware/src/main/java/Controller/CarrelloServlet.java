d80520144395fdcff4626c29e3b1b746134eb29b
edo247265 <edoardodominicis27@gmail.com>
2021-04-19 12:03:44 +0200

    doGet modifiche

--- DTHardware/src/main/java/Controller/CarrelloServlet.java
+++ DTHardware/src/main/java/Controller/CarrelloServlet.java
@@ -1,12 +1,16 @@
 package Controller;
 
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class CarrelloServlet
 {
-	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
-
+		RequestDispatcher dispatcher=request.getRequestDispatcher("");
+		dispatcher.forward(request, response);
 	}
 }

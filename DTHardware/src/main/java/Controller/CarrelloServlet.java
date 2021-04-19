ce56b0c9eb020941974e0bd2a1230f08d423939b
B3ta01 <f.tasuccio@gmail.com>
2021-04-19 12:09:59 +0200

    Dichiarata stringa e stampata

--- DTHardware/src/main/java/Controller/CarrelloServlet.java
+++ DTHardware/src/main/java/Controller/CarrelloServlet.java
@@ -5,11 +5,14 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
+import java.util.Locale;
 
 public class CarrelloServlet
 {
 	public void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
 	{
+		String s = "ciao";
+		System.out.println(s.toUpperCase(Locale.ROOT));
 		RequestDispatcher dispatcher=request.getRequestDispatcher("");
 		dispatcher.forward(request, response);
 	}

62e4232655764f55bdd81c9f63000d0f7550de42
edo247265 <edoardodominicis27@gmail.com>
2021-04-19 11:58:23 +0200

    Update CarrelloServlet.java

--- DTHardware/src/main/java/Controller/CarrelloServlet.java
+++ DTHardware/src/main/java/Controller/CarrelloServlet.java
@@ -1,6 +1,12 @@
 package Controller;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
 public class CarrelloServlet
 {
+	public void doGet(HttpServletRequest request, HttpServletResponse response)
+	{
 
+	}
 }

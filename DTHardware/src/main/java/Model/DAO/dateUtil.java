9301012d9dd19eb370ad140cf8c7e798a232387a
B3ta01 <f.tasuccio@gmail.com>
2021-05-06 11:06:33 +0200

    DATA CONVERT

--- DTHardware/src/main/java/Model/DAO/dateUtil.java
+++ DTHardware/src/main/java/Model/DAO/dateUtil.java
@@ -1,6 +1,14 @@
 package Model.DAO;
 
+import java.util.Calendar;
+
+
 public class dateUtil
 {
+    public static String PrepTime (Calendar data)  {
+
+        String stringDate = data.get(Calendar.YEAR)+"-"+ data.get(Calendar.MONTH)+"-"+ data.get(Calendar.DAY_OF_WEEK)+"-"+ data.get(Calendar.HOUR_OF_DAY);
+       return stringDate;
+    }
 	//2024-06-01 00:00:01
 }

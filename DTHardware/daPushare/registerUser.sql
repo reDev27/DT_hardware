712dca3512775cc0c1f8c02c22a3d8da5af61d22
edo247265 <edoardodominicis27@gmail.com>
2021-05-14 11:58:36 +0200

    register aggiornata

--- DTHardware/daPushare/registerUser.sql
+++ DTHardware/daPushare/registerUser.sql
@@ -6,17 +6,13 @@ create procedure registerUser(
     in passwIn varbinary(100),
     in nomeIn varchar(30),
     in cognomeIn varchar(30),
-    in ntelefonoIn int,
-    in dataacquistoIn timestamp,
-    in ncartaIn int,
-    in scadenzaIn timestamp,
-    in cvvIn int
+    in ntelefonoIn int
 )
 
 begin
 
     insert into cliente values
-    (usernameIn, emailIn, passwIn, nomeIn, cognomeIn, ntelefonoIn, dataacquistoIn, ncartaIn);
+    (usernameIn, emailIn, passwIn, nomeIn, cognomeIn, ntelefonoIn);
 
 end //
 DELIMITER ;

bec0282012ddee135baebfab61561f805bacc394
B3ta01 <f.tasuccio@gmail.com>
2021-05-15 16:31:41 +0200

    update procedure

--- DTHardware/src/main/java/Model/DAO/UserDAO.java
+++ DTHardware/src/main/java/Model/DAO/UserDAO.java
@@ -25,6 +25,7 @@ public class UserDAO extends BaseDAO
 		callableStatement.setString("scadenzaIn", DateUtil.PrepTime(scadenza));
 		callableStatement.setString("cvvIn", String.valueOf(cvv));
 		doExecute(callableStatement);
+
 	}
 
 	public void register(String username, String eMail, String password, String nome, String cognome, String nTelefono, String userType, String passData) throws SQLException
@@ -49,5 +50,60 @@ public class UserDAO extends BaseDAO
 		callableStatement.registerOutParameter("esito", Types.BOOLEAN);
 		return doExecute(callableStatement).getBoolean("esito");
 	}
+
+
+
+	public void InsertCategoria(String nome, int quantita, int codiceaBarre,String userType, String passData) throws SQLException
+	{
+		openConnection(userType, passData);
+		CallableStatement callableStatement=user.prepareCall("{call InsertCategoria(?, ?, ?)}");
+		callableStatement.setString("nomeIn", nome);
+		callableStatement.setString("quantitaIn",String.valueOf(quantita));
+		callableStatement.setString("codiceabarreIn",String.valueOf(codiceaBarre));
+		doExecute(callableStatement);
+
+	}
+	public void InsertCompone(int nprodotti,String userType, String passData) throws SQLException
+	{
+		openConnection(userType, passData);
+		CallableStatement callableStatement=user.prepareCall("{call InsertCompone(?)}");
+		callableStatement.setString("nprodottiIn", String.valueOf(nprodotti));
+		doExecute(callableStatement);
+
+	}
+	public void InsertIndirizzo(String via,int ncivico,String citta, int cap, boolean flag, String username, String userType, String passData) throws SQLException {
+		openConnection(userType, passData);
+		CallableStatement callableStatement = user.prepareCall("{call InsertIndirizzo(?, ?, ?, ?, ?, ?)}");
+		callableStatement.setString("viaIn", via);
+		callableStatement.setString("ncivicoIn", String.valueOf(ncivico));
+		callableStatement.setString("cittaIn", citta);
+		callableStatement.setString("capIn", String.valueOf(cap));
+		callableStatement.setString("flagIn", String.valueOf(flag));
+		callableStatement.setString("usernameIn", username);
+		doExecute(callableStatement);
+	}
+	public void InsertOrdine(int id, int sconto, double totale,String userType, String passData) throws SQLException {
+		openConnection(userType, passData);
+		CallableStatement callableStatement = user.prepareCall("{call InsertOrdine(?, ?, ?)}");
+		callableStatement.setString("idIn", String.valueOf(id));
+		callableStatement.setString("quantitaIn", String.valueOf(sconto));
+		callableStatement.setString("codiceabarreIn", String.valueOf(totale));
+		doExecute(callableStatement);
+	}
+	public void InsertProdotto(int codiceaBarre, double prezzo, String descrizione,String specifiche, int quantita,String marca, String modello,String userType, String passData) throws SQLException {
+		openConnection(userType, passData);
+		CallableStatement callableStatement = user.prepareCall("{call InsertProdotto(?, ?, ?, ?, ?, ?, ?)}");
+		callableStatement.setString("codiceabarreIn", String.valueOf(codiceaBarre));
+		callableStatement.setString("prezzoIn", String.valueOf(prezzo));
+		callableStatement.setString("descrizioneIn", descrizione);
+		callableStatement.setString("specificheIn", specifiche);
+		callableStatement.setString("quantitaIn", String.valueOf(quantita));
+		callableStatement.setString("marcaIn", marca);
+		callableStatement.setString("modelloIn", modello);
+		doExecute(callableStatement);
+	}
+
+
+
 
 }

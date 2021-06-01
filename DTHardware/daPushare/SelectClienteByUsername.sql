bbf05e8970ba7e27e12697cd4a45286383a7e6d7
B3ta01 <f.tasuccio@gmail.com>
2021-06-01 11:11:06 +0200

    add selectclientebyusername

---
+++ DTHardware/daPushare/SelectClienteByUsername.sql
@@ -0,0 +1,12 @@
+delimiter //
+drop procedure if exists SelectClienteByUsername;
+create procedure SelectClienteByUsername(
+    in usernameIn varchar(30)
+)
+begin
+
+    select (USERNAME,EMAIL,PASSW,NOME,COGNOME,NTELEFONO) from CLIENTE
+    where cliente.USERNAME=usernameIn;
+
+end //
+delimiter ;

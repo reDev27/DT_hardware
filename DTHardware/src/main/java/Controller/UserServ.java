230eedc61a7f59056475bbaf937c65a2396bd757
B3ta01 <f.tasuccio@gmail.com>
2021-06-11 12:21:13 +0200

    modify

--- DTHardware/src/main/java/Controller/UserServ.java
+++ DTHardware/src/main/java/Controller/UserServ.java
@@ -1,21 +1,12 @@
 package Controller;
 
-import Model.DAO.UserNotLoggedBean;
-import Model.Product;
-import Model.ProductsArray;
-import com.google.gson.Gson;
-import com.google.gson.JsonElement;
-import com.google.gson.JsonObject;
-
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import java.io.File;
 import java.io.IOException;
-import java.sql.SQLException;
 
 @WebServlet(name="UserServ" , value = "/UserServ")
 public class UserServ extends HttpServlet
